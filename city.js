c34b42c4bae80f7b51445cb192ce6f09feb0ff6c
Brandon <imbrandonj42@gmail.com>
2023-09-23 16:03:58 -0500

    `city.js` content completion

--- city.js
+++ city.js
@@ -20,9 +20,9 @@ switch ( pageTitle ) {
         words3 = 'Nashville was the first Confederate state capital to fall to Union forces during the Civil War in 1862.';
         break;
     case ('Memphis City Page'):
-        words1 = 'test1';
-        words2 = 'test2';
-        words3 = 'test3';
+        words1 = 'Memphis is the "Home of the Blues" and "Birthplace of Rock \'n\' Roll."';
+        words2 = 'Graceland was Elvis Presley\'s home in Memphis.';
+        words3 = 'Memphis is a major Mississippi River city.';
         break;
     case ('Gatlinburg City Page'):
         words1 = 'It\s nested just outside of the Great Smoky Mountains National Park.';
@@ -30,9 +30,9 @@ switch ( pageTitle ) {
         words3 = 'Escape the Gatlinburg area and explore the beautiful mountains.';
         break;
     case ('Knoxville City Page'):
-        words1 = 'test1';
-        words2 = 'test2';
-        words3 = 'test3';
+        words1 = 'It served as the capital of the Southwest Territory in the early 1800\'s.';
+        words2 = 'Knoxville is home to the University of Tennessee.';
+        words3 = 'The city has a thriving art and music scene, hosting film, art, and music festivals regularly.';
 }
 
 function typing(words, item) {
@@ -45,16 +45,16 @@ function typing(words, item) {
     }, 60);  // interval rate is typing speed
 }
 
-// type effect display sequentially:
+// type effect display sequentially
 // item1:
 typing(words1, item1);
 
 // wait for item1 completion
 setTimeout(function () {
     typing(words2, item2)
-    }, words1.length * 60);
+    }, words1.length * 60 + 400); // sequentual interval, allow words1 to finish + 400 delay
 
 // wait for item1 and item2 completion
 setTimeout(function () {
     typing(words3, item3)
-}, words1.length * 60 + words2.length * 60);  
+}, words1.length * 60 + words2.length * 60 + 800);  // words1 + words2 + 400 delay + 400 delay for this
